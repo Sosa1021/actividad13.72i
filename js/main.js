aa912a23165765a66c611d0b314114b51c72929a
Mateo Lohezic <mateolohezic@gmail.com>
2023-10-05 19:50:21 -0300

    Cambio de nombre de archivos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const animalesMezclados = ["perro", "perro", "sapo", "gato", "gato", "sapo", "perro", "gato", "sapo", "gato"];
+
 animalesMezclados.filter(animal => animal === 'perro').map (perro => console.log("Guau"));
 animalesMezclados.filter(animal => animal === 'gato').map (gato => console.log("Miau"));
 animalesMezclados.filter(animal => animal === 'sapo').map(sapo => console.log("Werep"));
